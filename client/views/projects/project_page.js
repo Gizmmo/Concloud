341911e9fa165cb3c0ae6d5ded7e6f79a979f894
DennyScott <DennyScott301@gmail.com>
2014-01-19 12:47:36 -0600

    SmartFile

--- client/views/projects/project_page.js
+++ client/views/projects/project_page.js
@@ -3,7 +3,7 @@ var sf = new SmartFile({});
 
 Template.projectPage.events({
 	/**
- 	 * This will mimic an actual update to the current project
+	 * This will mimic an actual update to the current project
 	 * @param  Meteor.call('updateProject', this._id, function (error, result) {		});	}} [description]
 	 * @return {[type]}   [description]
 	 */
@@ -17,17 +17,49 @@ Template.projectPage.events({
 		var file = template.find('#upload').files[0];
 		sf.upload(file,
 
- 		function (err, res){
-	    	if (err) {
-	        	console.log("upload failed", err);
-	        	return;
-	    	}
+			function (err, res){
+				if (err) {
+					console.log("upload failed", err);
+					return;
+				}
 
-	    	//Successful Upload
-	    	// var fileData = {
-	    	// 	name: 
-	    	// }
-	    	
+			//Successful Upload
+			
+			var folderCreation = {
+				createdByAuthorID : Meteor.user()._id,
+				createdByAuthorName : Meteor.user().profile.name,
+				createdDate : new Date()
+
+			};
+
+			var folderUpdate = {
+				updateDate : new Date(),
+				updateAuthorID : Meteor.user()._id,
+				updateAuthorName : Meteor.user().profile.name
+			};
+
+			var fileCreation = {
+				createdByAuthorID: Meteor.user()._id,
+				createdByAuthorName: Meteor.user().profile.name,
+				createdDate: new Date()
+			};
+
+			var fileData = {
+				name: file.name,
+				size: file.size,
+				fileCreation: fileCreation
+			};
+
+			var folderData = {
+				folderCreation : folderCreation,
+				file : fileData,
+				folderUpdate : folderUpdate
+			};
+
+			var myProject = Projects.findOne({_id: Session.get("currentProject")});
+			myProject.folders = folderData;
+			
+			
 		});
 	},
 

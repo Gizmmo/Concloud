cf230eec9e9dccfd8b20f337314acfbc01bd54e2
Gizmmo <travisscott301@gmail.com>
2014-01-21 20:04:52 -0600

    Manage Users UI Updated

--- client/views/admin/users/user_list.js
+++ client/views/admin/users/user_list.js
@@ -1,3 +1,4 @@
+var clickedID = null;
 Template.userList.helpers({
 	/**
 	 * Finds all projects
@@ -5,6 +6,41 @@ Template.userList.helpers({
 	 */
 	users: function() {
 		return Meteor.users.find({}).fetch();
+	},
+	convertedTime: function () {
+		if(this.profile.recent){
+			if(this.profile.recent.lastLogin){
+				return formatDate(this.profile.recent.lastLogin);
+			}
+		}
+		return formatDate(new Date().getTime());
+	}
+});
+
+Template.userList.events({
+	/**
+	 * This will mimic an actual update to the current project
+	 * @param  Meteor.call('updateProject', this._id, function (error, result) {		});	}} [description]
+	 * @return {[type]}   [description]
+	 */
+	'click .b-user-item': function () {
+		$("#first-name").text(this.profile.firstName);
+		$("#last-name").text(this.profile.lastName);
+		$("div.controls select").val(this.profile.userGroup);
+		$("#last-logged-label").text(formatDate(this.profile.recent.lastLogin));
+		if(this.profile.recent.lastProjectName){
+			$("#last-worked-label").text(this.profile.recent.lastProjectName);
+		} else{
+			$("#last-worked-label").text("None");
+		}
+		clickedID = this._id;
+	},
+	'click #update-user': function () {
+		var firstName = $("#first-name").val();
+		var lastName = $("#last-name").val();
+		var userGroup = $("#user-group").val();
+		Meteor.users.update({_id: clickedID}, {$set:{"profile.firstName": firstName, "profile.lastName": lastName, "profile.userGroup": userGroup}})
+		//Meteor.users.findOne({"_id" : clickedID});
 	}
 });
 
--- server/helpers/account_creation.js
+++ server/helpers/account_creation.js
@@ -5,3 +5,9 @@ Meteor.methods({
         return id;
      }
 });
+
+Meteor.users.allow({
+    update: function(userId, docs, fields, modifier) {
+        return true;
+    }
+});

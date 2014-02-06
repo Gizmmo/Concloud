cf83fa917d982831cb489c014718b91f23d9c442
DennyScott <dennyscott301@gmail.com>
2014-02-06 15:27:33 -0600

    Fixed notifications ahref

--- client/views/projects/project_page.js
+++ client/views/projects/project_page.js
@@ -133,14 +133,14 @@ Template.projectPage.events({
 	    var projectData = Projects.findOne({_id: Session.get("currentProject")});
 	    var folderData = getFolderData(projectData);
 	    if(!(folderTitle in folderData.folders)){
-		console.log("Inside create folder");
-		folderData.folders[folderTitle] = createFolder(folderTitle, folderTitle);
-		Meteor.call('createDirectory', getDirectoryFromStack(projectData, false) + folderTitle, function (error, result) {
-		    if(error)
-			console.log(error);
-		});
-		Meteor.call('updateProject', Session.get('currentProject'),projectData.folders);
-		Meteor.call('createProjectNotification', projectData, function (error, result) {});
+			console.log("Inside create folder");
+			folderData.folders[folderTitle] = createFolder(folderTitle, folderTitle);
+			Meteor.call('createDirectory', getDirectoryFromStack(projectData, false) + folderTitle, function (error, result) {
+			    if(error)
+				console.log(error);
+			});
+			Meteor.call('updateProject', Session.get('currentProject'),projectData.folders);
+			Meteor.call('createProjectNotification', projectData, function (error, result) {});
 	    }else{
 		 throwError("This folder already existed, please create a new folder name.");
 	    }

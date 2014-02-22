176895fa021726e4f2c71fe425fc61e347a6c80b
Gizmmo <travisscott301@gmail.com>
2014-02-21 21:44:41 -0600

    HR Field Deletion added

--- collections/hrData.js
+++ collections/hrData.js
@@ -18,5 +18,28 @@ Meteor.methods({
 		});
 
 		return projectID;
+	},
+
+	HRFieldUpdate: function (newEntry) {
+			var user = Meteor.user();
+			var entry = _.extend(_.pick(newEntry, 'fieldName', 'defaultValue'), {
+			createdOn: new Date().getTime(),
+			createdBy: user._id
+		});
+
+		var entryID = HRData.update({"_id" : newEntry._id}, newEntry);
+
+		return entryID;
+	},
+
+	HRFieldDelete: function (entryID) {
+		var fieldName = HRData.findOne({"_id" : entryID}).fieldName;
+		HRData.remove({"_id" : entryID});
+
+		hrFound = HR.find({});
+		hrFound.forEach(function (post) {
+			delete post[fieldName]; //LAST EDITING HERE
+			HR.update({userId: post.userId}, post);
+		});
 	}
 });

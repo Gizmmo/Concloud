d2db55585b1b17214f5ed62e596b41206942d333
DennyScott <dennyscott301@gmail.com>
2014-02-26 01:52:29 -0600

    Moved Dash board side entries to own template

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -40,65 +40,11 @@ Template.dashboard.helpers({
 		});
 	},
 
-	hrEntries: function (){
-		var hr = HR.findOne({userId: Meteor.user()._id});
-		if(HR.findOne({userId: Meteor.user()._id})){
-			return  HR.findOne({userId: Meteor.user()._id}).hrValues;
-		} else {
-			return [];
-		}
-	},
-
     removeHeight : function() {
 	    $('body').css('height', "auto");
-	},
-
-	userName : function() {
-		return Meteor.user().profile.firstName + " " + Meteor.user().profile.lastName ;
-	},
-
-	userGroup : function() {
-		return Meteor.user().profile.userGroup;
-	},
-
-	joinDate : function() {
-		return formatDate(Meteor.user().profile.joinDate);
-	},
-
-	lastLogin : function() {
-		var lastLogin = Meteor.user().profile.recent.lastLogin;
-
-		if(lastLogin === undefined || lastLogin === null)
-			return 'First Time';
-		return formatDate(lastLogin);
-	},
-
-	isProject : function(){
-		var lastProject = Meteor.user().profile.recent.lastProjectID;
-		if(lastProject === undefined || lastProject === null || lastProject === "None"){
-			return false;
-		}
-		return true;
-	},
-
-	lastProjectID : function() {
-		return Meteor.user().profile.recent.lastProjectID;
-	},
-
-	lastProject : function() {
-		var lastProject = Meteor.user().profile.recent.lastProjectName;
-		if(lastProject === undefined || lastProject === null || lastProject === "None")
-			return 'No Project';
-		return lastProject;
-	},
-
-	sickDays : function() {
-		return Meteor.user().profile.hr.sickDays;
-	},
-
-	vacationDays : function() {
-		return Meteor.user().profile.hr.vacationDays;
 	}
+
+	
 
 
 });

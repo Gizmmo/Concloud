c91fa14636fedbe78d8e305565dcaf4d3ecdbdb5
DennyScott <dennyscott301@gmail.com>
2014-03-05 18:35:02 -0600

    Added subtrades password entrance, close #268, close #259

---
+++ client/views/projects/subtradepassword/subtradepassword.js
@@ -0,0 +1,15 @@
+Template.subtradepassword.events({
+	'click #enter-project': function () {
+		var project = Projects.findOne({_id: this._id});
+		Session.set('thisId', this._id);
+		project.password = $('#project-password').val();
+		Meteor.call('checkPassword', project, function(err, res){
+			if(res){
+			Router.go('projectPage', {"_id": Session.get('thisId')});
+		}else{
+			$('#incorrectPassword').removeClass('hidealert');
+		}
+		});
+		
+	}
+});

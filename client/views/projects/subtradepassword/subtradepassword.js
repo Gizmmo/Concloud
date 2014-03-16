15fbecc30e4bd9adbfeacfb47bfcae0a04e98ea6
Gizmmo <travisscott301@gmail.com>
2014-03-16 00:40:03 -0500

    Subtrade passwords can not user the enter button close #302

--- client/views/projects/subtradepassword/subtradepassword.js
+++ client/views/projects/subtradepassword/subtradepassword.js
@@ -1,6 +1,6 @@
 Template.subtradepassword.events({
 	'click #enter-project': function () {
-		var project = Projects.findOne({_id: this._id});
+				var project = Projects.findOne({_id: this._id});
 		Session.set('thisId', this._id);
 		project.password = $('#project-password').val();
 		Meteor.call('checkPassword', project, function(err, res){
@@ -9,7 +9,22 @@ Template.subtradepassword.events({
 		}else{
 			$('#incorrectPassword').removeClass('hidealert');
 		}
-		});
-		
+	});
+	},
+
+	 'keypress #project-password': function (event) {
+		//This will stop the default submitting of the form
+		if(event.which === 13){
+					var project = Projects.findOne({_id: this._id});
+		Session.set('thisId', this._id);
+		project.password = $('#project-password').val();
+		Meteor.call('checkPassword', project, function(err, res){
+			if(res){
+			Router.go('projectPage', {"_id": Session.get('thisId')});
+		}else{
+			$('#incorrectPassword').removeClass('hidealert');
+		}
+	});
+		}
 	}
 });
